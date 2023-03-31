5b8feace79e5a6e22b47854a02c1db6a789fd4de
SilantevR <craft_89@inbox.ru>
2023-03-31 20:47:34 +0300

    homework1 corrections

--- 002-console/task1/index.js
+++ 002-console/task1/index.js
@@ -5,26 +5,75 @@ const { hideBin } = require("yargs/helpers");
 
 yargs(hideBin(process.argv))
   .command({
+    command: "current",
+    describe: "дата и время в формате ISO вперед",
+    builder: {
+      date: {
+        alias: "d",
+        type: "boolean",
+        demandOption: false,
+        describe: "Количество дней",
+      },
+      month: {
+        alias: "m",
+        type: "boolean",
+        demandOption: false,
+        describe: "Количество месяцев",
+      },
+      year: {
+        alias: "y",
+        type: "boolean",
+        demandOption: false,
+        describe: "Количество лет",
+      },
+    },
+    handler({ date = false, month = false, year = false }) {
+      let time = new Date();
+      if (year) {
+        console.log(time.getFullYear());
+      } else if (month) {
+        console.log(time.getMonth() + 1);
+      } else if (date) {
+        console.log(time.getDate());
+      } else {
+        console.log(time.toISOString());
+      }
+    },
+  })
+  .parse();
+
+yargs(hideBin(process.argv))
+  .command({
     command: "add",
     describe: "дата и время в формате ISO вперед",
     builder: {
-      d: {
+      day: {
+        alias: "d",
         type: "number",
         demandOption: false,
         describe: "Количество дней",
       },
       month: {
+        alias: "m",
         type: "number",
         demandOption: false,
         describe: "Количество месяцев",
       },
+      year: {
+        alias: "y",
+        type: "number",
+        demandOption: false,
+        describe: "Количество лет",
+      },
     },
-    handler({ d = 0, month = 0 }) {
+    handler({ day = 0, month = 0, year = 0 }) {
       let date = new Date();
-      if (d) {
-        date.setDate(date.getDate() + d);
+      if (day) {
+        date.setDate(date.getDate() + day);
       } else if (month) {
         date.setMonth(date.getMonth() + month);
+      } else if (year) {
+        date.setFullYear(date.getFullYear() + year);
       }
       console.log(date.toISOString());
     },
@@ -36,41 +85,35 @@ yargs(hideBin(process.argv))
     command: "sub",
     describe: "дата и время в формате ISO вперед",
     builder: {
-      d: {
+      day: {
+        alias: "d",
         type: "number",
         demandOption: false,
         describe: "Количество дней",
       },
       month: {
+        alias: "m",
         type: "number",
         demandOption: false,
         describe: "Количество месяцев",
       },
+      year: {
+        alias: "y",
+        type: "number",
+        demandOption: false,
+        describe: "Количество лет",
+      },
     },
-    handler({ d = 0, month = 0 }) {
+    handler({ day = 0, month = 0, year = 0 }) {
       let date = new Date();
-      if (d) {
-        date.setDate(date.getDate() - d);
+      if (day) {
+        date.setDate(date.getDate() - day);
       } else if (month) {
         date.setMonth(date.getMonth() - month);
+      } else if (year) {
+        date.setFullYear(date.getFullYear() - year);
       }
       console.log(date.toISOString());
     },
   })
   .parse();
-
-const argv = yargs(hideBin(process.argv)).argv;
-
-const date = new Date();
-
-if (!argv._.length) {
-  if (argv.year || argv.y) {
-    console.log(date.getFullYear());
-  } else if (argv.month || argv.m) {
-    console.log(date.getMonth() + 1);
-  } else if (argv.date || argv.d) {
-    console.log(date.getDate());
-  } else {
-    console.log(date.toISOString());
-  }
-}
